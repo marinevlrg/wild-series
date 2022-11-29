beab35cdaa64f6add67b8b04eeb7467ac91b92a3
marinevlrg <marine.valorge@gmail.com>
2022-11-29 13:47:09 +0100

    quete 7 ok

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Drame',
+        'Animation',
+        'Horreur',
+        'Romance',
+        'Historique',
+        'Comédie',
+    ];
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+            $this->addReference('category_' . $categoryName, $category);
+        }
+        $manager->flush();
+    }
+}
